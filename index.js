235eaf7db3d0f8d9e758bc6977dcd988c067285b
Elton Lac <lacelton@yahoo.com>
2021-02-11 15:39:17 -0800

    Added questions

---
+++ index.js
@@ -0,0 +1,42 @@
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "project",
+    message: "What is the project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license does your project have?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What is the command to download npm files?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about this repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know before contributing?",
+  },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
